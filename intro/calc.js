25e53482cd962ed596fe2fb265d248e834ae6663
aniruddha <aniruddha.kudalkar@gmail.com>
2019-01-28 12:22:37 +0530

    cal : operations

---
+++ intro/calc.js
@@ -0,0 +1,7 @@
+var num1 = 100
+var num2 = 200
+var add = num1 + num2
+console.log('Addition is '+ add)
+console.log('Sunstrcation is ')
+console.log('Division is ')
+console.log('Multiplication is ')
