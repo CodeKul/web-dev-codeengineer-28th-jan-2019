2d080d204a6fda1c6f5a124f717903dc0a12a603
aniruddha <aniruddha.kudalkar@gmail.com>
2019-01-29 10:39:03 +0530

    fundamentals of js

---
+++ second/fundamentals.js
@@ -0,0 +1,20 @@
+/**
+ * this is multiline comment
+ * 
+ */
+// es5 <=
+'use strict'
+var myNm = 'Android'
+if(true) {
+    var myNm = 'GoLang'
+}
+console.log('My Name is '+myNm)
+
+//es6
+let nm = 'java'
+if(true) {
+    let nm = 'python'
+}
+console.log('Programming Language is '+nm)
+const PI = 3.14
+// jDGDjhsgdsj = 10
