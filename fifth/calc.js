5e40bcab4573c34278471aacf7ba9a3fc9ffab2a
aniruddha <aniruddha.kudalkar@gmail.com>
2019-02-01 10:36:39 +0530

    html : dom events

---
+++ fifth/calc.js
@@ -0,0 +1,17 @@
+function sum(num1, num2, curr, dvRDAVDzsges) {
+    console.log(num1)
+    console.log(num2)
+    console.log(curr)
+    dvRDAVDzsges.innerHTML = `<b>${parseInt(num1) + parseInt(num2)}</b>`
+}
+
+function sub(num1, num2) {
+
+}
+
+function mul(num1, num2) {
+
+}
+function div(num1, num2) {
+
+}
